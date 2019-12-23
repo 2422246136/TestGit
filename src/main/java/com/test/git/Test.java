5b781c78cf81b717dd855fd354332408fd30d62a
tom_glb <goodMorning_glb@atguigu.com>
2019-12-23 12:02:47 +0800

    说的

--- src/main/java/com/test/git/Test.java
+++ src/main/java/com/test/git/Test.java
@@ -2,10 +2,6 @@ package com.test.git;
 
 public class Test {
 public static void main(String[] args) {
-<<<<<<< HEAD
 	System.out.println("right");
-=======
-	System.out.println("left");
->>>>>>> branch 'master' of https://github.com/2422246136/TestGit.git
 }
 } 
